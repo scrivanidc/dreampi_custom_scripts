f65391424b323236fb94dd60b913152b5562c561
scrivanidc <scrivanidc@gmail.com>
2025-01-24 15:32:22 -0300

    Update dcnet_on_off.sh
    code improvements

--- dcnet_on_off.sh
+++ dcnet_on_off.sh
@@ -6,7 +6,8 @@ echo ""
 echo "Choose option number"
 echo "1. DCNET Script ON"
 echo "2. DCNET Script OFF (Standard DreamPi)"
-read -p "Type 1 or 2 and press Enter: " opcao
+echo "3. Delete DCNET Files"
+read -p "Type 1, 2 or 3 and press Enter: " opcao
 
 cd /home/pi/dreampi/
 echo ""
@@ -14,8 +15,8 @@ verificar_arquivo_a() {
   if [ ! -f "dreampi_dcnet.py" ]; then
     echo "dreampi_dcnet.py backup and dcnet.rpi does not exist. Downloading..."
     echo ">>"
-    wget -O dreampi_dcnet.py https://github.com/flyinghead/flycast/raw/refs/heads/dev/tools/dreampi/dreampi.py
-    wget https://github.com/flyinghead/flycast/raw/refs/heads/dev/tools/dreampi/dcnet.rpi
+    wget -q --show-progress -O dreampi_dcnet.py https://github.com/flyinghead/flycast/raw/refs/heads/dev/tools/dreampi/dreampi.py
+    wget -q --show-progress -O dcnet.rpi https://github.com/flyinghead/flycast/raw/refs/heads/dev/tools/dreampi/dcnet.rpi
     chmod +x dreampi_dcnet.py dcnet.rpi
   else
     echo "dreampi_dcnet.py and dcnet.rpi exists. OK."
@@ -46,6 +47,14 @@ copiar_arquivo_b() {
   echo ">>"
 }
 
+deletar_arquivo_a() {
+  copiar_arquivo_b
+  rm dreampi_dcnet.py dcnet.rpi
+  echo "deleting DCNET files: dreampi_dcnet.py and dcnet.rpi"
+  echo "You're now able to download DCNET updated files at option 1"
+  echo ">>"
+}
+
 verificar_arquivo_b
 verificar_arquivo_a
 
@@ -55,8 +64,11 @@ if [ "$opcao" -eq 1 ]; then
 elif [ "$opcao" -eq 2 ]; then
   copiar_arquivo_b
   echo "Done. DCNET Script OFF (Standard DreamPi Script enabled)"
+elif [ "$opcao" -eq 3 ]; then
+  deletar_arquivo_a 
+  echo "Done. DCNET files deleted"
 else
-  echo "Invalid option. Please choose 1 or 2."
+  echo "Invalid option. Please choose 1, 2 or 3."
 fi
 
 sudo service dreampi restart &
